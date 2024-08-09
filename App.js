b548c11301b3e30ec9cccac119ec5ef59a38ad25
Ayan Mondal <ayan.official.mail.id@gmail.com>
2024-08-10 00:56:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import DesktopView from './components/DesktopView';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DesktopView />
+    </div>
+  );
+}
+
+export default App;
